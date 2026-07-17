1dca84ca72bc42eb688ad8ae11e0b006395614f6
Angel Mendiratta <angelmendiratta6@gmail.com>
2026-07-17 06:21:02 +0530

    Create config.js with API and YouTube settings
    Added configuration settings for API and YouTube.

---
+++ frontend/config.js
@@ -0,0 +1,15 @@
+
+const CONFIG = {
+    API_URL: "YOUR_API_URL",
+    YOUTUBE: {
+        videoId:       '70oYrSnRgoI',
+        controls:      0,   
+        fs:            0,   
+        rel:           0,  
+        modestbranding:1,   
+        cc_load_policy:0,   
+        autoplay:      0    
+    }
+};
+
+
