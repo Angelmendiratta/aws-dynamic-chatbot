8910668b6c6deb2a6ed724a55a4235efcf90ec0b
Angel Mendiratta <angelmendiratta6@gmail.com>
2026-07-20 18:09:28 +0530

    add custom tutorial video

--- frontend/config.js
+++ frontend/config.js
@@ -1,14 +1,18 @@
 
 const CONFIG = {
     API_URL: "YOUR_API_URL",
-    YOUTUBE: {
-        videoId:       '70oYrSnRgoI',
-        controls:      0,   
-        fs:            0,   
-        rel:           0,  
-        modestbranding:1,   
-        cc_load_policy:0,   
-        autoplay:      0    
+    // YOUTUBE: {
+    //     videoId:       '70oYrSnRgoI',
+    //     controls:      1,   // 1 = show controls, 0 = hide all
+    //     fs:            0,   // 0 = hide fullscreen button
+    //     rel:           0,   // 0 = no related videos
+    //     modestbranding:1,   // 1 = hide YouTube logo
+    //     cc_load_policy:0,   // 0 = subtitles off
+    //     autoplay:      0    // 0 = don't autoplay
+    // }
+    VIDEO: {
+        src:  'https://appliance-bot-angel.s3.ap-southeast-1.amazonaws.com/chatbot-tutorial.mp4',
+        type: 'video/mp4'
     }
 };
 
